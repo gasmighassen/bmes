0022f5e801346d89666c7b96f60d8c42f0331577
Gasmi <gasmi@Gasmis-MacBook-Air.local>
2023-06-02 14:41:41 +0100

    landing page portfolio and map

---
+++ client/src/components/LandingPortfolio/LandingPortfolio.tsx
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+import "../../_dist/LandingPortfolio.css";
+import { portfolioImages } from "../../data/PortfolioLandingImages";
+type Props = {};
+
+const LandingPortfolio = (props: Props) => {
+  const [mainImg, setMainImg] = useState(portfolioImages[0].url);
+  return (
+    <div className="bmes__landing__portfolio__section">
+      <h1>Portfolio -</h1>
+      <div className="bmes__landing__portfolio_wrapper">
+        {" "}
+        <div className="bmes__landing__portfolio__main_image">
+          <img src={mainImg} alt="" />
+        </div>
+        <div className="bmes__landing__portfolio__thumbnails">
+          {portfolioImages.map((image) => (
+            <div key={image.id} className="bmes__landing__thumbnail">
+              <img
+                src={image.url}
+                alt=""
+                onClick={() => setMainImg(image.url)}
+              />
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default LandingPortfolio;
